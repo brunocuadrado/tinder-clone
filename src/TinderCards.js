168cb280cbf918b0747ed440cadfc7e172c98a5f
brunocuadrado <brunoncuadrado@gmail.com>
2021-12-10 15:28:42 +0100

    Agregando TinderCards

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
+import TinderCard from "react-tinder-card";
+import './TinderCards.css';
 
 function TinderCards() {
     const [people, setPeople] = useState([
 
         {
-            name: 'Steve Jobs',
-            url: ''
+            name: 'Mia Khalifa',
+            url: 'https://es.web.img3.acsta.net/r_1280_720/pictures/17/01/20/12/42/192403.jpg'
         },
         {
-            name: 'Mark Zuckerberg',
-            url: ''
+            name: 'Lana Rhoades',
+            url: 'https://horapiko.com/wp-content/uploads/2020/10/image-15.png'
         }
 
 
@@ -26,7 +28,21 @@ function TinderCards() {
     
     return (
         <div>
-            <h1>TinderCards</h1>
+            <div className='tinderCards__cardContainer'>
+                {people.map(person => (
+                    <TinderCard
+                        className="swipe"
+                        key={person.name}//Always use keys in React. Permite a React re-renderizar una lista de manera eficiente
+                        preventSwipe={['up', 'down']}
+                    >
+                        <div 
+                        style={{backgroundImage: `url(${person.url})`}}
+                        className="card">
+                            <h3>{person.name}</h3>
+                        </div>
+                    </TinderCard>
+                ))}
+            </div>
         </div>
     )
 }
